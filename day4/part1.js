0c20eaf63a0848a5c12cfcba816b279004b5bc93
Mo <33751344+V1254@users.noreply.github.com>
2020-12-11 04:30:14 +0000

    update logic for regex

--- day4/part1.js
+++ day4/part1.js
@@ -2,10 +2,9 @@ const data = require("./data");
 
 const PASSPORT_CODES = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
-const validRegex = /(?:byr|iyr|eyr|hgt|hcl|ecl|pid)\b/g;
-
+const regex = new RegExp(`(?:${PASSPORT_CODES.join("|")})\\b`, "g");
 const isValidPassport = (passport) => {
-  const matches = passport.match(validRegex);
+  const matches = passport.match(regex);
   return matches && PASSPORT_CODES.every((code) => matches.includes(code));
 };
 
